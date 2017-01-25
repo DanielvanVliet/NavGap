8683a3ab20920927d2dba0535c3e1e9b5db47d98
Danielvv1998 <danielvvliet@homail.com>
2017-01-25 13:46:14 +0000

    Updated bash program

--- navgapboot.sh
+++ navgapboot.sh
@@ -10,14 +10,14 @@ export USER HOME
 case "$1" in
 	start) ### only this is currently played
 		echo "### Updating List ###"
-		sudo iwlist wlan0 scan |grep ESSID > /home/pi/github/NavGap/log.csv
+		sudo iwlist wlan0 scan |grep -e Signal -e ESSID > /home/pi/github/NavGap/log.csv
 		;;
 	stop) ## update?
 		echo "### Stop Bootup ###"
 		echo "Stop Bootup" > /home/pi/navgap/log.csv
 		;;
 	update) ## updates list
-		sudo iwlist wlan0 scan |grep ESSID > /home/pi/navgap/log.csv
+		sudo iwlist wlan0 scan |grep -e Signal -e  ESSID > /home/pi/github/NavGap/log.csv
 		;;
 	*)
 		echo "### Usage: /ect/init.d/NavGapBoot {start|stop} ###"
