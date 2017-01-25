5c7ab1f30bfcf546e568240d7b36cff4548dc196
Danielvv1998 <danielvvliet@homail.com>
2017-01-25 10:22:35 +0000

    wifiDetection.py now creates a list with ssid's and their signal strengths

--- wifiDetection.py
+++ wifiDetection.py
@@ -3,13 +3,15 @@ import os
 import time
 
 conList = [
-	'MarcKYS',
-	'essidhere',
+        'MarcKYS',
+        'essidhere',
         'Connectify-me'
 ]
-tempList = []
+EssidList = []
+SignalList = []
+tlist = []
 connection = False
-updateCmd = 'sudo /home/pi/navgap/navgapboot.sh start'
+updateCmd = 'sudo bash /home/pi/github/NavGap/navgapboot.sh start'
 
 def checkConnection(essid):
         global connection
@@ -17,25 +19,44 @@ def checkConnection(essid):
                 #print('checking if {} is {}'.format(essid, each))
                 if essid == each:
                         connection = True
-                        print('######## DICHTBIJ POINT {} #########'.format(essid))
+                        #print('######## DICHTBIJ POINT {} #########'.format(essid))
                 elif connection == False:
-                        print('## No Connection ##')
+                        #print('## No Connection ##')
+                        connection = False
 
 #print('### updating log.csv ###')
 #os.system(updateCmd)
-
-while True:
-        time.sleep(1)
-        print('### Update ###')
+counter = 1
+#while True:
+while counter != 0:
+        time.sleep(0.1)
+        #print('### Update ###')
         os.system(updateCmd)
         with open('log.csv', 'r') as myCSVFile:
                 reader = csv.reader(myCSVFile)
+                row = 0
                 for each in reader:
-                        remove = '                  '
-                        tempList.append(each[0][27:-2])
-                        checkConnection(each[0][27:-1])           
+                        #print(each)
+                        row += 1
+                        if row % 2 == 1: #if even
+                                #print(each[0][48:52])
+                                EssidList.append(each[0][48:51])
+                        else:
+                                #print(each[0][27:-2])
+                                SignalList.append(each[0][27:-1])
+                                checkConnection(each[0][27:-1])           
+        counter -= 1
 
+        for essid in SignalList:
+                tlist.append([essid])                
+        count = 0
+        for signal in EssidList:
+                tlist[count].append(signal)
+                count +=1
+        print('### temp list ###')
+        print(tlist)
+print()
+print()
+for each in tlist:
+	print('{} : {}'.format(each[0],each[1]))
 
-
-print('### temp list ###')
-print(tempList)
