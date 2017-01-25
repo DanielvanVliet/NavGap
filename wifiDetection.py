37b4049d69ea2760dbca6ddb96b97a0e44de15b4
Michelbaartman <michelbaartman@gmail.com>
2017-01-25 13:56:14 +0100

    wifi update

--- wifiDetection.py
+++ wifiDetection.py
@@ -82,6 +82,7 @@ def changeNodeColor(canvas, node, color):
     canvas.itemconfig(node, fill=color)
 
 def createUI():
+    running = True
     x0, y0 = 10, 50
     x1, y1 = 50, 90
     x2, y2 = 90, 130
@@ -99,27 +100,47 @@ def createUI():
     o2 = canvas.create_oval(x1, x1, y1, y1, fill=blue, activefill=red)
     o3 = canvas.create_oval(x2, x2, y2, y2, fill=blue, activefill=red)
 
-    #exit = tkinter.Button(text='exit', command=lambda:)
-    #exit.grid()
+    def stopApp():
+        global running
+        running = False
+
+    counter = 0
+
+    exit = tkinter.Button(text='exit', command = stopApp())
+    exit.grid()
 
     change = tkinter.Button(text='change', command=lambda: changeNodeColor(canvas, o2, yellow))
     change.grid()
+
+    load = canvas.create_rectangle(100,100,counter,250)
     #canvas.create_oval()
     # STD stndard algoritme
 
+    curList = []
     #base.mainloop()
     base.update()
-    counter = 0
-    while True:
+    while running == True:
         time.sleep(0.1)
         print('update node color')
-        for each in tlist:
-            if each[0] == conList[2]:
-                changeNodeColor(canvas, o3, yellow)
+        print(counter)
+        canvas.coords(load, 100, 200, counter, 250)
+        for eachCon in conList:
+            for eachT in tlist:
+                #print('name: {}, signal: {}'.format(eachT[0], eachT[1]))
+                if eachT[0][0] == eachCon: # and eachT[1] > 40:
+                    curList.append(eachT[0], eachT[1], True)
+                    print('name: {}, signal: {}'.format(eachT[0], eachT[1]))
+
+                    changeNodeColor(canvas, o3, yellow)
+
+
         base.update_idletasks()
         base.update()
-        if counter >= 100:
-            break
+        #if counter <= 100:
+            #counter += 1
+            #running = False
+        if counter == 100:
+            running = False
         counter += 1
 
 createUI()
