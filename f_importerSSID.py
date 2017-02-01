9d252955dbc61a670106d860e0e3e8648f6c9693
Danielvv1998 <Danielvvliet@hotmail.com>
2017-02-01 19:58:21 +0100

    navgap db version

--- f_importerSSID.py
+++ f_importerSSID.py
@@ -1,18 +1,20 @@
 import pymysql.cursors
 
-connection = pymysql.connect(host='localhost',
-			     user='root',
-			     password='navgap',
-			     db='navgapdb')
+def import_SSID():
+	connection = pymysql.connect(host='192.168.0.2',
+					 user='monitor',
+					 password='navgap',
+					 db='navgapdb',
+					 charset='utf8mb4')
 
 
-curs = connection.cursor()
+	curs = connection.cursor()
 
-#Imports ssid from db into python dictionary
-spotDict = {}
-curs.execute("SELECT * FROM Locations")
-connection.commit()
+	#Imports ssid from db into python dictionary
+	spotDict = {}
+	curs.execute("SELECT * FROM Locations")
+	connection.commit()
 
-for row in curs.fetchall():
-	spotDict[row[0]] = [False, 0, row[1], row[2]]
-print(spotDict)
+	for row in curs.fetchall():
+		spotDict[row[0]] = [False, 0, row[1], row[2]]
+	return spotDict
