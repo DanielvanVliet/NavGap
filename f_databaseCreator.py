ca6efe9234b0a0d71634d2fa851f82e13ec419cf
Danielvv1998 <Danielvvliet@hotmail.com>
2017-02-02 16:27:23 +0100

    docstrings added

--- f_databaseCreator.py
+++ f_databaseCreator.py
@@ -10,72 +10,123 @@ connection = pymysql.connect(host='localhost',
 curs = connection.cursor()
 
 def add_lines(location1, location2, distance):
+	"""
+	adds line to LocationConnections
+	:param location1:
+	wifi-ssid1
+	:param location2:
+	wifi-ssid that 1 connects to
+	:param distance:
+	distance between ssid1 and ssid2
+	:return:
+	"""
 	location1 = "'" + location1 + "'"
 	location2 = "'" + location2 + "'"
 	query = "INSERT INTO LocationConnections values("+location1+', '+location2+", "+distance+")"
 	print("Query = "+query)
 	curs.execute(query)
 	connection.commit()
-	print("next connection between location")
+	print("Next connection between location")
 	print()
 
 def add_lines2(ssid, x ,y):
-	ssid = "'" + ssid + "'"
-	print(ssid)
-	query = "INSERT INTO Locations values("+ssid+", "+ str(x)+ ", "+ str(y)+ ")"
-	print("Query = "+query)
-	curs.execute(query)
-	connection.commit()
-	print("next locaton")
+    """
+    adds Location to Locations
+    :param ssid:
+    ssid name of that node
+    :param x:
+    x value on canvas
+    :param y:
+    y value on canvas
+    :return:
+    """
+    ssid = "'" + ssid + "'"
+    query = "INSERT INTO Locations values("+ssid+", "+ str(x)+ ", "+ str(y)+ ")"
+    print("Query = "+query)
+    curs.execute(query)
+    connection.commit()
+    print("Next location")
 
 def drop_table(name):
-	query = "DROP TABLE "+name
-	print("Query = "+query)
-	curs.execute(query)
-	connection.commit()
+    """
+    Function to easily drop a table
+    :param name:
+    name of table
+    :return:
+    """
+    query = "DROP TABLE "+name
+    print("Query = "+query)
+    curs.execute(query)
+    connection.commit()
 
 def create_locationConnections():
-	query = "CREATE TABLE LocationConnections(Location1 text, Location2 text, Afstand NUMERIC)"
-	print("Query = "+query)
-	curs.execute(query)
-	connection.commit()
-	print("Table LocationConnections created")
+    """
+    Creates new table LocationConnections
+    :return:
+    """
+    query = "CREATE TABLE LocationConnections(Location1 text, Location2 text, Afstand NUMERIC)"
+    print("Query = "+query)
+    curs.execute(query)
+    connection.commit()
+    print("Table LocationConnections created")
 
 def create_locations():
-	query = "CREATE TABLE Locations(SSID text, xvalue text, yvalue text)"
-	print("Query = "+query)
-	curs.execute(query)
-	connection.commit()
-	print("Table Locations created")
+    """
+    Creates new table Locations
+    :return:
+    """
+    query = "CREATE TABLE Locations(SSID text, xvalue text, yvalue text)"
+    print("Query = "+query)
+    curs.execute(query)
+    connection.commit()
+    print("Table Locations created")
 
 def add_locationConnections(number):
-	counter = 0
-	while counter != number:
-		L1 = input("Wifi-SSID1? ")
-		L2 = input("Wifi-SSID2? ")
-		D = input("Afstand tussen Wifi-SSID1 en Wifi-SSID2? ")
-		counter += 1
-		add_lines(L1, L2, D)
+    """
+    Asks for input to add a line to the table LocationConnections
+    :param number:
+    Number of lines that will be added
+    :return:
+    """
+    counter = 0
+    while counter != number:
+        L1 = input("Wifi-SSID1? ")
+        L2 = input("Wifi-SSID2? ")
+        D = input("Afstand tussen Wifi-SSID1 en Wifi-SSID2? ")
+        counter += 1
+        add_lines(L1, L2, D)
 
 def add_location(number):
-	counter = 0
-	while counter != number:
-		W = input("Wifi-SSID for this location? ")
-		X = input("X for this location? ")
-		Y = input("Y for this location? ")
-		counter += 1
-		add_lines2(W, X, Y)
+    """
+    Asks for input to add a line to the table Locations
+    :param number:
+    Number of lines that will be added
+    :return:
+    """
+    counter = 0
+    while counter != number:
+        W = input("Wifi-SSID for this location? ")
+        X = input("X for this location? ")
+        Y = input("Y for this location? ")
+        counter += 1
+        add_lines2(W, X, Y)
 
 def displaytable(table):
-	query = "SELECT * FROM "+str(table)
-	print("Query = "+query)
-	curs.execute(query)
-	connection.commit()
-	print()
-	for row in curs.fetchall():
-		 print(str(row[0])+" 	"+str(row[1])+" 	"+
-		 str(row[2]))		
-	print()
+    """
+    Displays a table from the database
+    :param table:
+    Name of the table that has to be displayed
+    :return:
+    """
+    query = "SELECT * FROM "+str(table)
+    print("Query = "+query)
+    curs.execute(query)
+    connection.commit()
+    print()
+    for row in curs.fetchall():
+         print(str(row[0])+" 	"+str(row[1])+" 	"+
+         str(row[2]))
+    print()
 
 while True:
 	print("Option 1: create table Locations")
